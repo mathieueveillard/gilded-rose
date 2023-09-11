0ad3e30679251487a115c7fc8ae8b9f0da39f992
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-09-11 14:37:41 +0200

    🔊 add console logs

--- src/index.ts
+++ src/index.ts
@@ -23,20 +23,24 @@ export class GildedRose {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
+            console.log(`This item : ${this.items[i].name} got removed 1 quality number and now has the following quality: ${this.items[i].quality}.`)
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
+                console.log(`This item : ${this.items[i]} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
               }
             }
             if (this.items[i].sellIn < 6) {
+              console.log(`${this.items[i].name}`)
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
+                console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
               }
             }
           }
@@ -44,6 +48,7 @@ export class GildedRose {
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
+        console.log(`This item : ${this.items[i].name} got removed 1 sellin number and now has the following sellin: ${this.items[i].sellIn}.`)
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
@@ -51,6 +56,7 @@ export class GildedRose {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
+                console.log(`This item : ${this.items[i].name} got removed 1 quality number and now has the following quality: ${this.items[i].quality}.`)
               }
             }
           } else {
@@ -59,6 +65,7 @@ export class GildedRose {
         } else {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
+            console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
           }
         }
       }
