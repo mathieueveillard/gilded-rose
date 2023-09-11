b9787d18b9eef215e6894f6ee2784f04139bdf73
Clément <95907390+clementpnn@users.noreply.github.com>
2023-09-11 16:01:48 +0200

    optimisazion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export class Item {
   constructor(
-      public name: string,
-      public sellIn: number,
-      public quality: number
+    public name: string,
+    public sellIn: number,
+    public quality: number
   ) {}
 }
 
@@ -10,59 +10,51 @@ export class GildedRose {
   constructor(public items: Item[] = []) {}
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-            console.log(`This item : ${this.items[i].name} got removed 1 quality number and now has the following quality: ${this.items[i].quality}.`)
+    for (let item of this.items) {
+      switch (item.name) {
+        case "Aged Brie":
+          if (item.quality < 50) {
+            item.quality += 1
+            console.log(`This item : ${item.name} got added 1 quality number and now has the following quality: ${item.quality}.`)
           }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log(`This item : ${this.items[i]} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              console.log(`${this.items[i].name}`)
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-                console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
-              }
-            }
+          break
+
+        case "Backstage passes to a TAFKAL80ETC concert":
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1
           }
-        }
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1
+          }
+          if (item.sellIn <= 0) {
+            item.quality = 0
+          }
+          break
+
+        case "Sulfuras, Hand of Ragnaros":
+          // No quality or sellIn changes
+          continue
+
+        default:
+          if (item.quality > 0) {
+            item.quality -= 1
+            console.log(`This item : ${item.name} got removed 1 quality number and now has the following quality: ${item.quality}.`)
+          }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-        console.log(`This item : ${this.items[i].name} got removed 1 sellin number and now has the following sellin: ${this.items[i].sellIn}.`)
+
+      if (item.name !== "Sulfuras, Hand of Ragnaros") {
+        item.sellIn -= 1
+        console.log(`This item : ${item.name} got removed 1 sellIn day and now has the following sellIn: ${item.sellIn}.`)
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-                console.log(`This item : ${this.items[i].name} got removed 1 quality number and now has the following quality: ${this.items[i].quality}.`)
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-            console.log(`This item : ${this.items[i].name} got added 1 quality number and now has the following quality: ${this.items[i].quality}.`)
-          }
-        }
+
+      if (item.sellIn < 0 && item.name !== "Aged Brie" && item.name !== "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0) {
+        item.quality -= 1
+        console.log(
+          `This item : ${item.name} got removed 1 quality number after sellIn days ended and now has the following quality: ${item.quality}.`
+        )
       }
     }
 
-    return this.items;
+    return this.items
   }
 }
