6d1ae860230d895786106f28494f0da8b35868a2
Clément <95907390+clementpnn@users.noreply.github.com>
2023-09-11 16:10:50 +0200

    update lisibility

--- src/index.ts
+++ src/index.ts
@@ -9,52 +9,67 @@ export class Item {
 export class GildedRose {
   constructor(public items: Item[] = []) {}
 
-  updateQuality() {
+  updateQuality(): Item[] {
     for (let item of this.items) {
-      switch (item.name) {
-        case "Aged Brie":
-          if (item.quality < 50) {
-            item.quality += 1
-            console.log(`This item : ${item.name} got added 1 quality number and now has the following quality: ${item.quality}.`)
-          }
-          break
+      this.QualityItem(item)
+    }
+    return this.items
+  }
 
-        case "Backstage passes to a TAFKAL80ETC concert":
-          if (item.sellIn < 11 && item.quality < 50) {
-            item.quality += 1
-          }
-          if (item.sellIn < 6 && item.quality < 50) {
-            item.quality += 1
-          }
-          if (item.sellIn <= 0) {
-            item.quality = 0
-          }
-          break
-
-        case "Sulfuras, Hand of Ragnaros":
-          // No quality or sellIn changes
-          continue
-
-        default:
-          if (item.quality > 0) {
-            item.quality -= 1
-            console.log(`This item : ${item.name} got removed 1 quality number and now has the following quality: ${item.quality}.`)
-          }
-      }
-
-      if (item.name !== "Sulfuras, Hand of Ragnaros") {
-        item.sellIn -= 1
-        console.log(`This item : ${item.name} got removed 1 sellIn day and now has the following sellIn: ${item.sellIn}.`)
-      }
-
-      if (item.sellIn < 0 && item.name !== "Aged Brie" && item.name !== "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0) {
-        item.quality -= 1
-        console.log(
-          `This item : ${item.name} got removed 1 quality number after sellIn days ended and now has the following quality: ${item.quality}.`
-        )
-      }
+  private QualityItem(item: Item) {
+    switch (item.name) {
+      case "Aged Brie":
+        this.QualityAgedBrie(item)
+        break
+      case "Backstage passes to a TAFKAL80ETC concert":
+        this.QualityBackstagePass(item)
+        break
+      case "Sulfuras, Hand of Ragnaros":
+        // No quality or sellIn changes
+        break
+      default:
+        this.QualityRegularItem(item)
     }
 
-    return this.items
+    this.SellInItem(item)
+  }
+
+  private QualityAgedBrie(item: Item) {
+    if (item.quality < 50) {
+      item.quality += 1
+      console.log(`This item : ${item.name} got added 1 quality number and now has the following quality: ${item.quality}.`)
+    }
+  }
+
+  private QualityBackstagePass(item: Item) {
+    if (item.sellIn < 11 && item.quality < 50) {
+      item.quality += 1
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality += 1
+    }
+    if (item.sellIn <= 0) {
+      item.quality = 0
+    }
+  }
+
+  private QualityRegularItem(item: Item) {
+    if (item.quality > 0) {
+      item.quality -= 1
+      console.log(`This item : ${item.name} got removed 1 quality number and now has the following quality: ${item.quality}.`)
+    }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 1
+      console.log(
+        `This item : ${item.name} got removed 1 quality number after sellIn days ended and now has the following quality: ${item.quality}.`
+      )
+    }
+  }
+
+  private SellInItem(item: Item) {
+    if (item.name !== "Sulfuras, Hand of Ragnaros") {
+      item.sellIn -= 1
+      console.log(`This item : ${item.name} got removed 1 sellIn day and now has the following sellIn: ${item.sellIn}.`)
+    }
   }
 }
