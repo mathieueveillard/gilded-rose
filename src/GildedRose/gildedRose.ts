c427c4c662a26161fe255d361bead991f37d2764
Dounia138 <97449294+Dounia138@users.noreply.github.com>
2023-09-12 10:36:22 +0200

    architecture + console log + tests

---
+++ src/GildedRose/gildedRose.ts
@@ -0,0 +1,69 @@
+import { Item } from "../Items/item";
+import { ItemType, Quality, SellIn } from "../Enums/enum";
+
+export class GildedRose {
+  items: Array<Item>;
+
+  constructor(items: Array<Item> = []) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    for (const item of this.items) {
+      console.log(`Before updateQuality: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+
+      switch (item.name) {
+        case ItemType.AgedBrie:
+          this.updateAgedBrie(item);
+          break;
+        case ItemType.BackstagePasses:
+          this.updateBackstagePasses(item);
+          break;
+        case ItemType.Sulfuras:
+          break;
+        default:
+          this.updateStandardItem(item);
+          break;
+      }
+
+      console.log(`After updateQuality: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+    }
+
+    return this.items;
+  }
+
+  private updateAgedBrie(item: Item) {
+    console.log(`Before updateAgedBrie: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+
+    item.sellIn -= 1;
+    item.quality = Math.min(Quality.MAX, item.quality + Quality.INCREASE);
+
+    console.log(`After updateAgedBrie: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+  }
+
+  private updateBackstagePasses(item: Item) {
+    console.log(`Before updateBackstagePasses: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+
+    item.sellIn -= 1;
+
+    if (item.sellIn < 0) {
+      item.quality = Quality.MIN;
+    } else if (item.sellIn <= SellIn.WARNING) {
+      item.quality = Math.min(Quality.MAX, item.quality + Quality.INCREASE_DOUBLE);
+    } else if (item.sellIn <= SellIn.SOON) {
+      item.quality = Math.min(Quality.MAX, item.quality + Quality.INCREASE);
+    }
+
+    console.log(`After updateBackstagePasses: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+  }
+
+  private updateStandardItem(item: Item) {
+    console.log(`Before updateStandardItem: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+
+    item.sellIn -= 1;
+    const decreaseAmount = item.sellIn < 0 ? Quality.DECREASE * 2 : Quality.DECREASE;
+    item.quality = Math.max(Quality.MIN, item.quality - decreaseAmount);
+
+    console.log(`After updateStandardItem: name=${item.name}, sellIn=${item.sellIn}, quality=${item.quality}`);
+  }
+}
